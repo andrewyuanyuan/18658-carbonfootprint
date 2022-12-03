cb8b7ee44cdfe4295d12f221e03603537032807e
Anyuan Yu <49481919+andrewyuanyuan@users.noreply.github.com>
2022-12-02 17:49:33 -0800

    1. fix warning of icon svg style

--- src/icons/emissonquota.js
+++ src/icons/emissonquota.js
@@ -4,8 +4,8 @@ import React from 'react';
 const EmissionquotaIcon = createSvgIcon(
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M4 3V2C4 0.895431 4.89543 0 6 0H12C13.1046 0 14 0.89543 14 2V3H17C17.5523 3 18 3.44772 18 4C18 4.55228 17.5523 5 17 5H16V16C16 17.6569 14.6569 19 13 19H5C3.34315 19 2 17.6569 2 16V5H1C0.447716 5 0 4.55228 0 4C0 3.44772 0.447716 3 1 3H4ZM6 2H12V3H6V2ZM4 5H14V16C14 16.5523 13.5523 17 13 17H5C4.44772 17 4 16.5523 4 16V5Z"
       fill="#818181"
     />
