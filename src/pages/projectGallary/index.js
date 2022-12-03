93cd2115103c25d50851d528b7ca31e06c2001f7
annieqxx <annieqxx@gmail.com>
2022-12-02 19:26:52 -0800

    fix flexbox

--- src/pages/projectGallary/index.js
+++ src/pages/projectGallary/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import HeaderLayout from '../../common/header';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+
+// import flex from '@react-css/flex'
+
  
+// const styles = {
+//   row: {
+//     display: flex,
+//   },
+  
+//   column: {
+//     flex: "33.33%",
+//     padding: "5px",
+//   },
+// };
 
 function ProjectGallary() {
   return (
@@ -9,17 +24,19 @@ function ProjectGallary() {
     <div>
       <img style={{ width: "100%" }} src="/static/images/projects/proj_gal_topimgtxt.png" alt="glarrybanner"/>
     </div>
-    <div class="row">
-  <div class="column">
-    <img src="/static/images/projects/proj_gal_img1.png" alt="forest" style={{ width: "100%" }} />
-  </div>
-  <div class="column">
-    <img src="/static/images/projects/proj_gal_oceanimg.png" alt="ocean" style={{ width: "100%" }} />
-  </div>
-  <div class="column">
-    <img src="/static/images/projects/proj_gal_img3.png" alt="other" style={{ width: "100%" }} />
-  </div>
-</div>
+
+    <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          p: 1,
+          m: 1,
+        }}
+      >
+        <img src="/static/images/projects/proj_gal_img1.png" alt="forest" style={{ width: "33.33%" }}  />
+        <img src="/static/images/projects/proj_gal_oceanimg.png" alt="ocean" style={{ width: "33.33%" }}  />
+        <img src="/static/images/projects/proj_gal_img3.png" alt="other" style={{ width: "33.33%" }}  />
+      </Box>
       
     </HeaderLayout>
   );
