4aca2e430c57e3e85d0d5704e1120c0b9c620c33
louis <liuzizhen1234@gmail.com>
2022-12-02 19:04:26 -0800

    centralize project

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import ProjectCard from './component/ProjectCard';
 import HeaderLayout from '../../common/header';
-import { Box, Container, Grid, Typography } from '@mui/material';
-import { useParams } from 'react-router-dom';
+import { Box, Container, Grid } from '@mui/material';
+import { useParams } from "react-router-dom";
 
 function Projects() {
   let { category } = useParams();
-  let projects = JSON.parse(localStorage.getItem('projects'))[category];
+  let projects = JSON.parse(localStorage.getItem("projects"))[category]
   return (
     <HeaderLayout>
-      <Container maxWidth={false}>
-        <Box sx={{ pt: 3 }}>
-          <Typography sx={{ mb: 3 }} variant="h4">
-            Category: {category}
-          </Typography>
-
+      <Container maxWidth='lg'>
+        <Box sx={{ pt: 6 }}>
           <Grid container spacing={3}>
-            {Object.keys(projects).map((id, _) => {
-              return (
-                <Grid item key={id} xs={3}>
-                  <ProjectCard
+            {
+              Object.keys(projects).map((id, _) => {
+                return <Grid item key={id} xs={3}>
+                  <ProjectCard 
                     id={id}
                     name={projects[id].name}
                     description={projects[id].description}
@@ -27,8 +23,8 @@ function Projects() {
                     category={category}
                   />
                 </Grid>
-              );
-            })}
+              })
+            }
           </Grid>
         </Box>
       </Container>
