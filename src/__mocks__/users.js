944ac8a20ba1400bb9bd3f4a3f9470477e1bab14
Zhaoqing Li <zhaoqinl@andrew.cmu.edu>
2022-12-04 12:34:56 -0800

    add attributes to transactions

--- src/__mocks__/users.js
+++ src/__mocks__/users.js
@@ -6,6 +6,10 @@ var users = {
     notification: false,
     transactions: [
       {
+        id: "1d135d5e-4932-46fa-9fee-4b3066c753a4",
+        ref: "CDD4645",
+        projectName: 'Clean Ocean',
+        type: 'deposit',
         name: 'ocean/cleanocean',
         amount: 3000,
         date: '9/20/2021',
@@ -22,6 +26,10 @@ var users = {
     projects: ['ocean/cleanocean', 'ocean/cleanocean1'],
     transactions: [
       {
+        id: "809f7636-a8ae-4e84-bd5d-8b6805e17da2",
+        ref: "CDD4645",
+        projectName: 'Clean Ocean',
+        type: 'deposit',
         name: 'ocean/cleanocean',
         amount: 3000,
         date: '9/20/2021',
