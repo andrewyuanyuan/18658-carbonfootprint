dfbd9ec2f80b26aa7fc98d6a62c74c4ee7a422dc
Donglai He <donglaih@andrew.cmu.edu>
2022-12-03 20:38:12 -0800

    fix post/edit project

--- src/pages/postProject/index.js
+++ src/pages/postProject/index.js
@@ -1,31 +1,53 @@
 import { Grid, Box } from '@mui/material';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import HeaderLayout from '../../common/header';
 import EditProjectDetail from './component/EditProjectDetail';
 
 
 function PostProject() {
-  const info = localStorage.getItem('FIXME');
-  // TODO: find project info from given type&name
-  // TODO: fix image in EditProjectDetail
-  // TODO: cancel button in EditProjectDetail
+  // TODO: edit entry connection
+  const { category, id } = useParams();
+  let props = {};
+
+  if (category === undefined) {
+    props = {
+      name: '',
+      type: '',
+      pricing: '',
+      country: '',
+      city: '',
+      image: '',
+      description: ''
+    }
+  } else {
+    const currProject = JSON.parse(localStorage.getItem('projects'))[category][id];
+    console.log(currProject);
+    props = {
+      name: id,
+      type: category,
+      pricing: currProject.pricing,
+      country: currProject.country,
+      city: currProject.city,
+      image: currProject.image,
+      description: currProject.description
+    }
+  }
 
   return (
     <Box component='main' sx={{ flexGrow: 1, py: 8, m: 4}}>
       <HeaderLayout>
         <Grid container xs={12}>
           <EditProjectDetail 
-            name='cleanocean'
-            type='ocean'
-            pricing='pricing'
-            country='China'
-            city='city'
-            image='/static/images/projects/ocean/CleanOcean.png'
-            description='description'
-            contact='contact'
+            name={props.name}
+            type={props.type}
+            pricing={props.pricing}
+            country={props.country}
+            city={props.city}
+            image={props.image}
+            description={props.description}
           />
         </Grid>
-
       </HeaderLayout>
     </Box>
   );
